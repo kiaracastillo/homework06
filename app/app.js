ba73ac6439cb81845f99aa6cf9dceb409ecabb91
kiaracastillo <kiaracastillom@gmail.com>
2023-10-28 16:28:13 -0400

    commit

--- app/app.js
+++ app/app.js
@@ -1,26 +1,4 @@
-import { changePage } from "./model.js";
-
-function route (){
-    let hashTag = window.location.hash;
-    let pageID = hashTag.replace("#", "");
-    let path = pageID.split("/");
-  console.log("hash ", hashTag);
-   console.log("page name " + pageID);
-   console.log("sub page name ", path);
-  changePage(path);
-   
-}
-
-function initListeners() {
-    console.log("hello");
-}
-
-function initSite() {
-   $(window).on("hashchange", route);
-   route();
-}
-
-$(document).ready(function () {
-    initListeners();
-    initSite();  
+$(".hamburger-icon").on("click", () => {
+    $(".hamburger-icon").toggleClass("open");
+  
 });
--- app/model.js
+++ app/model.js
@@ -1,39 +1,29 @@
-//var parentPage = "";
-export function changePage(pathArray){ 
-    //console.log("mvc");
-     $("#bread-crumb").html(``);
- 
-     if (pathArray == ""){
-       //  $("#app").html("home");
-         $.get(`pages/home.html`, (data) => {
-             $("#app").html(data);
-         }).fail(() => {
-             console.log("failed");
-         });
-     } else {
-         
-         if (pathArray.length == 1){
-             $.get(`pages/${pathArray}.html`, (data) => {
-                 if (data){
-                     $("#app").html(data);
-                 } else {
-                     alert("page not found")
-                 }
-         }).fail(() =>{
-             console.log("page not found");
-         });
-     }else {
-         $("#bread-crumb").html(`<a href="#${pathArray[0]}">${pathArray[0]}</a> / ${pathArray[1]}`);
-         $.get(`pages/${pathArray[1]}.html`, (data) =>{
-             if (data){
-                 $("#app").html(data);
-             }else {
-                 alert("page not found");
-             }
-         }).fail(() => {
-             console.log("page not found");
-         });
-     }
- }
- 
- }
+export var wordArray = [ "home", ]; 
+
+export function changeRoutes() { 
+    let hashTag = window.location.hash;
+    let pageID = hashTag.replace("#", ""); 
+
+
+    if (pageID != "") { 
+
+        $.get(`pages/${pageID}/${pageID}.html`, function (data)
+        {
+            console.log("page " + data);
+            $("#app").html(data);
+        } );
+      console.log(pageID);
+
+    } else {
+        $.get(`pages/home/home.html`, function (data) {
+            console.log("page " + data);
+            $("#app").html(data);
+        });
+
+        console.log("home");
+    }
+   
+    
+
+
+}
